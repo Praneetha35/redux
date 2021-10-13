e0320ae6e9e24d6acb87521eec5d489aa1c46009
Praneetha35 <praneethacp35@gmail.com>
2021-10-13 11:12:30 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { createStore } from 'redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//declaring a state as empty array
+function rootReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD ONE':
+      return state.concat('added one')
+    case 'SUBTRACT ONE':
+      return state.concat('subtracted one')
+    default:
+      return state
+  }
 }
 
-export default App;
+//A reducer is passed into it - a function which contains the switch statement
+let store = createStore(rootReducer)
+class App extends Component {
+  constructor() {
+    super()
+    store.subscribe(() => {
+      console.log("Subscribing to store")
+      console.log(store.getState())
+    })
+  }
+
+  increment() {
+    console.log('in store increment method')
+    store.dispatch({ type: 'ADD ONE', data: "added one" })
+  }
+
+  decrement() {
+    console.log('in store decrement method')
+    store.dispatch({ type: 'SUBTRACT ONE', data: "subtracted one" })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Redux counter</h1>
+        <button onClick={this.increment}> Increment </button>
+        <button onClick={this.decrement}> Decrement </button>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import change from "./counter";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+    change
+})
+
+export default rootReducer
